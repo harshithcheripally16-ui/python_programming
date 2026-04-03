70be69423dc630d24894ca7fb31fc4abf7309dbb
Harshith Cheripally <harshithcheripally16@gmail.com>
2026-04-03 14:18:50 +0530

    Refactor number checks to use input() for user input

--- day-04-homework.py
+++ day-04-homework.py
@@ -1,45 +1,45 @@
 #1. Check if number is zero or non-zero
-num = 0
+num = int(input())
 if num == 0:
     print("The number is zero")
 else:
     print("The number is non zero")
 
 #2. Check if number is +ve or -ve
-num = -3
+num = int(input())
 if num > 0:
     print("The number is +ve number")
 else:
     print("The number is -ve number")
 
 #3. Check if number is even or odd
-num = 2
+num = int(input())
 if num%2 == 0:
     print("The number is an even number")
 else:
     print("The number is an odd number")
 
 #4. Check if a person is eligible to vote or not
-num = 14
+num = int(input())
 if num >= 18:
     print("The person is eligible to vote")
 else:
     print("The person is a minor")
 
 #5. Check if number is divisible by 3 or not
-num = 7
-if num % 3:
+num = int(input())
+if num % 3 ==0:
     print("The number is divisible by 3")
 else:
     print("The number is not divisible by 3")
 
 #6. Check if number is divisible by 3 and 5 or not
-num = 30
-if num % 3 and num % 5:
+num = int(input())
+if num % 3==0 and num % 5==0:
     print("The number is divisible by 3 and 5")
-elif num % 3:
+elif num % 3==0:
     print("The number is only divisible by 3")
-elif num % 5:
+elif num % 5==0:
     print("The number is only divisible by 5")
 else:
     print("The number is not divisible by 3 and 5")
